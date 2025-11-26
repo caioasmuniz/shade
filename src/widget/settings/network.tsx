24d2b714dff6757e77aaa92001e6c8899ba18374
caioasmuniz <caiomuniz888@gmail.com>
2025-11-25 23:00:23 -0300

    add password handling

--- src/widget/settings/network.tsx
+++ src/widget/settings/network.tsx
@@ -1,7 +1,7 @@
 import Adw from "gi://Adw?version=1";
 import AstalNetwork from "gi://AstalNetwork?version=0.1";
 import Gtk from "gi://Gtk?version=4.0";
-import { createBinding, createComputed, For } from "gnim";
+import { createBinding, createComputed, createState, For } from "gnim";
 
 export default () => {
   const network = AstalNetwork.get_default()
@@ -12,6 +12,21 @@ export default () => {
   ], (wifi, wired, primary) =>
     primary === AstalNetwork.Primary.WIRED ?
       wired : wifi)
+  const [password, setPassword] = createState("")
+
+  const ApWidget = (ap: AstalNetwork.AccessPoint) =>
+    <Adw.ExpanderRow
+      iconName={ap.iconName}
+      title={ap.ssid}>
+      <Gtk.PasswordEntry onNotifyText={self =>
+        setPassword(self.text)} />
+      <Adw.ButtonRow
+        title={"Connect"}
+        visible = {ap.requiresPassword}
+        onActivated={() =>
+          ap.activate(password.get())}
+      />
+    </Adw.ExpanderRow>
 
   return <Adw.PreferencesPage name={"network"}>
     <Adw.PreferencesGroup
@@ -36,17 +51,7 @@ export default () => {
       <For each={createBinding(network.wifi, "accessPoints")
         .as(aps => aps.sort((a, b) => b.strength - a.strength))
       }>
-        {(ap: AstalNetwork.AccessPoint) =>
-          <Adw.ExpanderRow
-            iconName={ap.iconName}
-            title={ap.ssid}>
-            <Adw.ButtonRow
-              title={"Connect"}
-              onActivated={() =>
-                ap.activate()}
-            />
-          </Adw.ExpanderRow>
-        }
+        {ApWidget}
       </For>
     </Adw.PreferencesGroup>
   </Adw.PreferencesPage>
